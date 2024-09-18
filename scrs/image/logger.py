285869fd7f863a8e3fc1e1b910de8bc0761f8f69
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2024-09-18 17:30:01 +1000

    Ruff updates

--- scrs/image/logger.py
+++ scrs/image/logger.py
@@ -6,20 +6,23 @@ if TYPE_CHECKING:
     from logging import Logger
 
 
-
 def get_log_and_save(logger: "Logger") -> callable:
     """
     Returns a decorator to log and save the history of an image.
     """
+
     def decorator(func):
         """
         A decorator to return self from a method.
         """
+
         def wrapper(instance: "Image", *args, **kwargs) -> Any:
             ret = func(instance, *args, **kwargs)
             logger.info(f"Ran {func.__name__} on image.")
 
             instance.save_snapshot(func.__name__)
             return ret
+
         return wrapper
+
     return decorator
