ff2d26365634a8857b7bc3b5902de8d14916f077
Benjamin van de Vorstenbosch <bencvdv@gmail.com>
2024-09-18 17:29:10 +1000

    Image history all stored in single fits file

--- scrs/image/image.py
+++ scrs/image/image.py
@@ -7,7 +7,6 @@ from astropy.io import fits
 
 import numpy as np
 
-from scrs.utils import ensure_dir
 from .logger import get_log_and_save
 
 
@@ -15,19 +14,24 @@ logger = getLogger(__name__)
 log_and_save = get_log_and_save(logger)
 
 
-
 class Image:
     _data: np.ndarray = None  # The image data being manipulated
+    _save_history: bool = False  # Whether to save history
+    _save_file: Path = None  # The path to save the image to
+    _history: list[str] | None = None  # The history of operations
 
-    _history: list[str] = None  # A list of all operations performed on the image
-    _save_path: Path = None  # The path to save the image to
+    def __init__(self, data: np.ndarray = None):
+        if data is not None:
+            self.set_data(data)
+
+        self._history = []
 
     @property
     def data(self) -> np.ndarray:
         """
         Provides a deep copy of the data to ensure the Image data is not modified,
         so that history can be retained.
-        
+
         If you want to applying operations on the Image data, there are two options available:
         1. Use this property to get a deep copy, and then use `set_data` to update the image with your changes.
         2. Use `apply` to apply operations to the data without requiring a deepcopy.
@@ -37,17 +41,6 @@ class Image:
         """
         return deepcopy(self._data)
 
-    @property
-    def history(self) -> list[str]:
-        """
-        Provides a copy of the history (for read-only purposes).
-
-        Returns:
-            list[str]: A list of all operations performed on the image.
-        """
-        return [h for h in self._history]
-
-
     @log_and_save
     def load_fits(self, fits_file: Path) -> Self:
         """
@@ -55,7 +48,7 @@ class Image:
 
         Args:
             fits_file (Path): The path to the fits file to load.
-        
+
         Returns:
             Self: The current instance of the image.
 
@@ -64,7 +57,7 @@ class Image:
         """
         if not fits_file.exists():
             raise FileNotFoundError(f"File {fits_file} does not exist")
-        
+
         if self._data is not None:
             logger.warning("Call to `load_fits` is overwriting existing data!")
 
@@ -73,30 +66,10 @@ class Image:
         return self
 
     @log_and_save
-    def load_history(self, path: Path, step_id: str) -> Self:
-        """
-        Loads an image from the history.
-
-        Args:
-            path (Path): The path to the history folder.
-            step_id (str): The ID of the step to load from the history.
-
-        Returns:
-            Self: The current instance of the image.
-        """
-        ensure_dir(path)
-
-        for file in path.iterdir():
-            if step_id in file.name:
-                return Image().load_fits(file)
-
-        raise FileNotFoundError(f"No file found for with ID {step_id}!.")
-
-    @log_and_save
     def set_data(self, data: np.ndarray) -> Self:
         """
         Sets the image data to the given data.
-        
+
         Args:
             data (np.ndarray): The data to set the image to.
 
@@ -105,11 +78,11 @@ class Image:
         """
         if self._data is not None:
             logger.warning("Call to `set_data` is overwriting existing data!")
-        
+
         self._data = data
 
         return self
-    
+
     @log_and_save
     def apply(self, func: Callable[[np.ndarray], np.ndarray]) -> Self:
         """
@@ -124,71 +97,34 @@ class Image:
         """
         if self._data is None:
             raise ValueError("Cannot apply operations to empty data.")
-        
+
         self._data = func(self._data)
 
         return self
-    
+
     @log_and_save  # Required to save anything up to this point
-    def use_history(self, enable: bool = True, path: Path | None = None) -> Self:
+    def enable_history(self, path: Path | None = None, enable: bool = True) -> Self:
         """
-        Enable history for this image. All interim operations will be saved
-        within the given folder.
+        Enable history for this image. All operations from this point will be saved
+        within the given fits file.
 
         Args:
             enable (bool): Whether to enable history.
-            path (Path): The path to save the history to.
-        
-        Returns:
-            Self: The current instance of the image.
-        """
-        if path is not None:
-            if self._save_path is not None:
-                logger.warning("Overwriting existing save path.")
-            
-            ensure_dir(path, create=True)
-            self._save_path = path
-
-        if enable:
-            if self._history is None:
-                self._history = []
-            else:
-                logger.warning("History is already enabled.")
-
-            if self._save_path is None:
-                raise ValueError("Cannot enable history without a save path.")
-        else:
-            self._history = None
-        
-        return self        
-        
-    
-    def clear_history(self, delete_files: bool = False) -> Self:
-        """
-        Clears the history of this image. To save a copy of the data after clearing
-        the history, call save_snapshot directly.
-
-        Be warned; if delete_files is called, copies of this will also
-        have the copied part of their history cleared. This is to prevent
-        accidental dangling references.
-
-        Args:
-            delete_files (bool): Whether to delete the files saved in the history.
+            path (Path): The file path to save the history to.
 
         Returns:
             Self: The current instance of the image.
         """
-        if self._save_path is None:
-            return self
-        
-        if delete_files:
-            for h in self._history:
-                for file in self._save_path.iterdir():
-                    if h in file.name:
-                        file.unlink()
-                        break
-        
-        self._history = []
+        self._save_history = enable
+
+        if enable and path is None and self._save_file is None:
+            raise ValueError("Cannot enable history without a save path.")
+
+        if path:
+            if self._save_file is not None:
+                logger.warning("Path was already set! Saving to a new file.")
+
+            self._save_file = path
 
         return self
 
@@ -197,14 +133,32 @@ class Image:
         Takes a copy of the current image and saves it to the history.
         """
         # Check that history is enabled
-        if self._history is None or self._data is None:
+        if not self._save_history or self._data is None:
             return
-        
-        # Save the image
-        step_id = str(uuid4())[0:8]
-        hdu = fits.PrimaryHDU(self._data)
-        hdu.writeto(self._save_path / f"({step_id}) {step_name}.fits")
+
+        step_id = str(uuid4())
+        history_note = f"[{step_id}] {step_name}"
+
+        if self._save_file.exists() and len(self._history) > 0:
+            # There is history, so this must be part of the pipeline. Append to the existing file.
+            hdu_list = fits.open(self._save_file)
+
+            hdr = hdu_list[0].header
+            hdr.append(("HISTORY", history_note))
+
+            # Set the new data as the primary HDU. Shift the previous primary HDU to the second HDU.
+            new_hdu = fits.PrimaryHDU(self._data, header=hdr)
+            prev_hdu = fits.ImageHDU(hdu_list.pop(0).data)
+
+            hdu_list = fits.HDUList([new_hdu, prev_hdu] + hdu_list)
+
+        else:
+            # Save the image as a new file
+            hdr = fits.Header({"HISTORY": history_note})
+            hdu = fits.PrimaryHDU(self._data, header=hdr)
+            hdu_list = fits.HDUList([hdu])
+
+        hdu_list.writeto(self._save_file, overwrite=True)
 
         self._history.append(step_id)
-
         logger.info(f"Saved history [{step_id}] for {step_name}")
